835ffbc8f3dd11ac6d4d7e6c9e52c6c20b66cc86
tr4ce <141042999+tracemoepy@users.noreply.github.com>
2023-09-09 01:33:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from pyrogram import Client, idle
-
-
-app = Client("tracemoepy")
-
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+app = Client("john_bot",
+             api_id=config.get('pyrogram', 'api_id'),
+             api_hash=config.get('pyrogram', 'api_hash'),
+             bot_token=config.get('pyrogram', 'bot_token'),
+             plugins=dict(root="plugins"),
+)
 if __name__ == '__main__':
     app.start()
     idle()
+    
